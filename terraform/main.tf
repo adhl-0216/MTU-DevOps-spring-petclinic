eb2a10ac7a261fb8965ecaf9364fd2abc9f71b03
ADHL <hanlim2002@gmail.com>
2025-04-05 01:41:12 +0000

    ci-cd changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,20 @@ module "app" {
   cluster_id         = var.environment == "staging" ? module.infra.staging_cluster_id : module.infra.prod_cluster_id
   log_group_name     = var.environment == "staging" ? module.infra.staging_log_group_name : module.infra.prod_log_group_name
 }
+
+# Expose infra module outputs at the root level
+output "staging_cluster_id" {
+  value = module.infra.staging_cluster_id
+}
+
+output "prod_cluster_id" {
+  value = module.infra.prod_cluster_id
+}
+
+output "staging_log_group_name" {
+  value = module.infra.staging_log_group_name
+}
+
+output "prod_log_group_name" {
+  value = module.infra.prod_log_group_name
+}
