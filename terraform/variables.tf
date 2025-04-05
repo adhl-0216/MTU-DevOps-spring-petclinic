a9d2c3b0591a9c5167e4d68046bc2eee648298ef
ADHL <hanlim2002@gmail.com>
2025-04-05 02:09:03 +0000

    terraform variable update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,13 @@ variable "dockerhub_username" {
   type    = string
   default = ""
 }
+
+variable "log_group_name" {
+  type    = string
+  default = ""
+}
+
+variable "cluster_id" {
+  type    = string
+  default = ""
+}
