2514ab7daca78821c685100455d1cc1e7bf26db4
ADHL <hanlim2002@gmail.com>
2025-04-06 07:18:40 +0000

    export output to root

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,7 @@ output "staging_log_group_name" {
 output "prod_log_group_name" {
   value = module.infra.prod_log_group_name
 }
+
+output "task_definition_arn" {
+  value = module.app.task_definition_arn
+}
